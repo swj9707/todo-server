2b1e74298cc53b7406f184adfecea2dcee6c926f
swj9707 <swj1265@naver.com>
2022-10-02 00:06:56 +0900

    feature/server-initsetup Entity Setup

--- src/main/java/com/example/todoserver/entity/TodoEntity.java
+++ src/main/java/com/example/todoserver/entity/TodoEntity.java
@@ -1,15 +1,28 @@
 package com.example.todoserver.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name = "Todo")
 public class TodoEntity {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
     private String id;
     private String userId;
     private String title;
